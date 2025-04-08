d0cd664e2670687c5d028f07f4ca2b1bce240d00
Harison <harisoncom@gmail.com>
2025-04-08 17:44:34 +0100

    Using env variables instead of hard coding

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 # main.py
 from fastapi import FastAPI
 import psycopg2
+import os
+from dotenv import load_dotenv
 
 from app.models.FragranceHouse import FragranceHouse
 
 app = FastAPI()
 
+load_dotenv()
+
 # Database connection details
-DB_HOST = "postgres-db-test"
-DB_NAME = "scentmatch_test_db"
-DB_USER = "scentmatch_user"
-DB_PASSWORD = "scentmatch_password"
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 
 @app.get("/user/{user_id}")
