fd14b5a390ddfd621cafbdb15cb38a26d611dfd5
mrmuffinman <harisoncom@gmail.com>
2025-07-02 11:17:21 +0100

    Postgres test fix
    Changed password env name previously however test .env wasn't updated.

--- scripts/wait_for_postgres.py
+++ scripts/wait_for_postgres.py
@@ -7,7 +7,7 @@ def wait():
     host = os.getenv("DB_HOST")
     dbname = os.getenv("DB_NAME")
     user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
+    password = os.getenv("POSTGRES_PASSWORD")
     port = os.getenv("DB_PORT")
 
     while True:
